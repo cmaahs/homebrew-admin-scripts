13e245f2130d98bddeee85aeee2df7b1351589a9
Christopher Maahs <cmaahs@gmail.com>
2019-10-24 14:32:02 +0000

    Initial fix for install

--- Formula/get-kubectlclient.rb
+++ Formula/get-kubectlclient.rb
@@ -1,4 +1,4 @@
-class GetKubectlClient < Formula
+class GetKubectlclient < Formula
   desc "This script fetches the latest Kubectl client that matches the cluster you are connected to, and replaces the /usr/local/bin/kubectl symlink"
   homepage "https://github.com/cmaahs/homebrew-admin-scripts"
   url "https://github.com/cmaahs/homebrew-admin-scripts.git"
