b93217797af288979ba5674c8fab3e77e5771555
Christopher Maahs <cmaahs@gmail.com>
2020-02-22 18:05:32 +0000

    Fixing name

--- Formula/get-helmclient.rb
+++ Formula/get-helmclient.rb
@@ -1,8 +1,8 @@
-class GetKubectlclient < Formula
-  desc "This script fetches the latest Kubectl client that matches the cluster you are connected to, and replaces the /usr/local/bin/kubectl symlink"
+class GetHelmclient < Formula
+  desc "This script fetches the latest Helm client that matches the cluster you are connected to, and replaces the /usr/local/bin/helm symlink"
   homepage "https://github.com/cmaahs/homebrew-admin-scripts"
   url "https://github.com/cmaahs/homebrew-admin-scripts.git"
-  version "0.0.1"
+  version "0.0.2"
 
   def install
     bin.install "bin/get-helmclient"
