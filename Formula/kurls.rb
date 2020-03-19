2273c40768506f23761c7553059051a1bd461afd
Christopher Maahs <cmaahs@gmail.com>
2020-03-19 23:41:20 +0000

    Mar2020 Update4 - Nik Tools

--- Formula/kurls.rb
+++ Formula/kurls.rb
@@ -1,8 +1,8 @@
-class KurlsScripts < Formula
+class Kurls < Formula
   desc "This is a tool to generate URLs for Splice Machine K8s clusters"
   homepage "https://github.com/cmaahs/homebrew-admin-scripts"
   url "https://github.com/cmaahs/homebrew-admin-scripts.git"
-  version "0.0.1"
+  version "0.0.2"
 
   def install
     bin.install "bin/kurls"
--- Formula/use-env.rb
+++ Formula/use-env.rb
@@ -1,8 +1,8 @@
-class UseEnvScripts < Formula
+class UseEnv < Formula
   desc "This is a tool to switch contexts for Splice Machine K8s clusters"
   homepage "https://github.com/cmaahs/homebrew-admin-scripts"
   url "https://github.com/cmaahs/homebrew-admin-scripts.git"
-  version "0.0.1"
+  version "0.0.2"
 
   def install
     bin.install "bin/use-env"
