47a60fe6a3f6b289301c82b443ddc5ae56e1333c
Christopher Maahs <cmaahs@gmail.com>
2020-03-19 23:26:51 +0000

    Mar2020 Update3 - Nik Tools

---
+++ Formula/kurls.rb
@@ -0,0 +1,10 @@
+class KurlsScripts < Formula
+  desc "This is a tool to generate URLs for Splice Machine K8s clusters"
+  homepage "https://github.com/cmaahs/homebrew-admin-scripts"
+  url "https://github.com/cmaahs/homebrew-admin-scripts.git"
+  version "0.0.1"
+
+  def install
+    bin.install "bin/kurls"
+  end
+end
