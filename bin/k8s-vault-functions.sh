f4e16d2ca3f7482b4d50393a09ab2950c4539efd
Christopher Maahs <cmaahs@gmail.com>
2020-07-23 02:41:58 +0000

    July 2020 U1, some updates

--- bin/k8s-vault-functions.sh
+++ bin/k8s-vault-functions.sh
@@ -3,29 +3,16 @@
 function connect_k8s_vault {
 
     AWSPROFILE=${AWS_PROFILE:-splice-nonprod}
-    CSP=$(kubectl -n default get secret/vault-key-store -o json | jq -r '.data.CLOUD_PROVIDER' | base64 -d)
-    vaultname="$(kubectl get secret vault-key-store -o json | jq -r .data.ENVIRONMENT | base64 -d | tr -d '-')vault"
-    SPLICE_ENV=$(kubectl get secret vault-key-store -o json | jq -r .data.ENVIRONMENT | base64 -d)
-    nohup kubectl port-forward  $(kubectl get vaultserver/vault -n default -o json | jq -r '.status.vaultStatus.active') 8200:8200 -n default >/dev/null 2>&1 &
+    CSP=$(kubectl -n splice-system get secret/vault-key-store -o json | jq -r '.data.CLOUD_PROVIDER' | base64 -d)
+    vaultname="$(kubectl -n splice-system get secret vault-key-store -o json | jq -r .data.ENVIRONMENT | base64 -d | tr -d '-')vault"
+    SPLICE_ENV=$(kubectl -n splice-system get secret vault-key-store -o json | jq -r .data.ENVIRONMENT | base64 -d)
+    export VAULT_TOKEN=$(kubectl -n splice-system get secrets vault-keys -o json | jq -r '.data."vault-root-token"' | base64 -d)
+    nohup kubectl port-forward  $(kubectl get vaultserver/vault -n splice-system -o json | jq -r '.status.vaultStatus.active') 8200:8200 -n splice-system >/dev/null 2>&1 &
     export PORT_FORWARD_PID=$!
     export VAULT_SKIP_VERIFY="true"
     export VAULT_ADDR='https://127.0.0.1:8200'
-    if [[ "${CSP}" == "aws" ]]; then
-      rt_key="$(kubectl get secret vault-key-store -o json | jq -r .data.ENVIRONMENT | base64 -d)vault"
-      ENC_TOKEN=$(aws ssm get-parameters --profile ${AWSPROFILE} --with-decryption --names "/k8s/${rt_key}-root-token" | jq -r '.Parameters[0].Value')
-      export VAULT_TOKEN=$(aws kms decrypt --profile ${AWSPROFILE} --ciphertext-blob fileb://<(echo "${ENC_TOKEN}" | base64 -d -) --output text --query Plaintext --encryption-context 'Tool=vault-unsealer' | base64 -d -)
-    elif [[ "${CSP}" == "az" ]]; then
-      export VAULT_TOKEN=$(az keyvault secret show --name "vault-root-token" --vault-name "${vaultname}" | jq -r '.value' | base64 -d)
-    elif [[ "${CSP}" == "gcp" ]]; then
-      LOCATION=$(gcloud container clusters list --format='value(location)' --filter="name=${SPLICE_ENV}")
-      export VAULT_TOKEN=$(gsutil cat "gs://${SPLICE_ENV}-vault-keys/vault-root-token" | gcloud kms decrypt --location ${LOCATION} --keyring ${SPLICE_ENV}-keyring --key ${SPLICE_ENV}-vault-key --ciphertext-file - --plaintext-file -)
-    else
-      echo "Could not determine ROOT TOKEN"
-      exit 1
-    fi
     sleep 2
     vault kv list secret
-
 }
 
 function disconnect_k8s_vault {
