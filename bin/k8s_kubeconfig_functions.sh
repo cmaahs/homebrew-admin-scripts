1a8d78e45e548623325c1fb91297ad790e1e9289
Christopher Maahs <cmaahs@gmail.com>
2020-08-25 18:04:03 +0000

    support multi panes

--- bin/k8s_kubeconfig_functions.sh
+++ bin/k8s_kubeconfig_functions.sh
@@ -4,8 +4,10 @@ function set-default-configfile {
     cat ~/.kubeconfig_default | grep ${ITERM_PROFILE} > /dev/null 2>&1
     if [[ $? != 0 ]]; then
         echo "${ITERM_PROFILE}:${1}" >> ~/.kubeconfig_default
+        echo "${ITERM_PROFILE}-12:${1}" >> ~/.kubeconfig_default
     else
-        sed -i "/${ITERM_PROFILE}/c ${ITERM_PROFILE}:${1}" ~/.kubeconfig_default
+        sed -i "/${ITERM_PROFILE}$/c ${ITERM_PROFILE}:${1}" ~/.kubeconfig_default
+        sed -i "/${ITERM_PROFILE}-12$/c ${ITERM_PROFILE}-12:${1}" ~/.kubeconfig_default
     fi
 }
 
