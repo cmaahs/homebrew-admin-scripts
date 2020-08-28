a685521175dbb031c301fe1cc5d6a2a3246478b3
Christopher Maahs <cmaahs@gmail.com>
2020-08-28 02:35:50 +0000

    Adding some tab titles

--- Formula/util-scripts.rb
+++ Formula/util-scripts.rb
@@ -2,7 +2,7 @@ class UtilScripts < Formula
   desc "This is a collection of general utility scripts"
   homepage "https://github.com/cmaahs/homebrew-admin-scripts"
   url "https://github.com/cmaahs/homebrew-admin-scripts.git"
-  version "0.0.22"
+  version "0.0.23"
 
   def install
     bin.install "bin/auth-vault"
