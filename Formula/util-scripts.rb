365523df3ee1dab325e3309c232d2b1c97a5605c
Christopher Maahs <cmaahs@splicemachine.com>
2020-10-22 12:03:51 -0500

    Adjust pane sizes

--- Formula/util-scripts.rb
+++ Formula/util-scripts.rb
@@ -2,7 +2,7 @@ class UtilScripts < Formula
   desc "This is a collection of general utility scripts"
   homepage "https://github.com/cmaahs/homebrew-admin-scripts"
   url "https://github.com/cmaahs/homebrew-admin-scripts.git"
-  version "0.0.32"
+  version "0.0.33"
 
   def install
     bin.install "bin/auth-vault"
