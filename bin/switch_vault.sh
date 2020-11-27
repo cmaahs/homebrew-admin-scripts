373928606863983479fc10ad390850a972726899
Christopher Maahs <cmaahs@gmail.com>
2020-11-27 13:12:51 +0000

    vault token update

--- bin/switch_vault.sh
+++ bin/switch_vault.sh
@@ -6,7 +6,6 @@ if [[ -z ${WHICH_VAULT} ]]; then
   WHICH_VAULT=build
 fi
 
-
 echo "Switching to ${WHICH_VAULT}"
 export VAULT_ADDR=$(security find-generic-password -l "${WHICH_VAULT}_vault_addr" -w automation.keychain-db)
 export VAULT_TOKEN=$(security find-generic-password -l "${WHICH_VAULT}_vault_token" -w automation.keychain-db)
