f3058f4792092e05ca4ef6cf3d50cf95a081a799
Christopher Maahs <cmaahs@gmail.com>
2021-02-11 23:44:40 +0000

    worktree functions, take 2

--- bin/worktree_functions.sh
+++ bin/worktree_functions.sh
@@ -18,7 +18,7 @@ function cdwt {
   NONE='\033[0m'
   SEDHOME=$(echo ${HOME} | sed 's/\//\\\//g')
   WTPATH=$(find ~/Worktrees -maxdepth 3 -mindepth 3 | grep -v .DS_Store | sed "s/${SEDHOME}\/Worktrees\///" | fzf)
-  if [[ -n ${WTPATH} ]]; then 
+  if [[ -n ${WTPATH} ]]; then
     cd ${HOME}/Worktrees/${WTPATH}
     export BRANCH=$(git branch --show-current)
   else
@@ -27,11 +27,16 @@ function cdwt {
 }
 
 function mkwt {
+  JIRA=${1}
+  BRANCH=${2}
+  if [[ -z ${JIRA} || -z ${BRANCH} ]]; then
+    echo -e "${YELLOW}Need JIRA (DBAAS-nnnn) and BRANCH (short name)${NONE}"; exit 1
+  fi
   YELLOW='\033[01;33m'
   NONE='\033[0m'
   REPONAME=$(basename $(git config --get remote.origin.url) | sed 's/\.git//')
-  if [[ ! -d ~/Worktrees/${REPONAME}/${1} ]]; then
-    git worktree add -b ${1} ~/Worktrees/${REPONAME}/${1}
+  if [[ ! -d ~/Worktrees/${JIRA}/${REPONAME}/${2} ]]; then
+    git worktree add -b ${1}/${2} ~/Worktrees/${JIRA}/${REPONAME}/${2}
   else
     echo -e "${YELLOW}The worktree directory already exists, aborting${NONE}"
   fi
@@ -45,23 +50,24 @@ function rmwt {
   CURRDIR=$(pwd)
   if [[ "${PARENT}" == "${CURRDIR}" ]]; then
     REPONAME=$(basename $(git config --get remote.origin.url) | sed 's/\.git//')
-    WTPATH=$(find ~/Worktrees/${REPONAME} -maxdepth 2 -mindepth 2 | grep -v .DS_Store | sed "s/${SEDHOME}\/Worktrees\///" | fzf)
-    if [[ -n ${WTPATH} ]]; then 
+    WTPATH=$(find ~/Worktrees/**/${REPONAME} -maxdepth 1 -mindepth 1 | grep -v .DS_Store | sed "s/${SEDHOME}\/Worktrees\///" | fzf)
+    if [[ -n ${WTPATH} ]]; then
       if [[ -d ~/Worktrees/${WTPATH} ]]; then
         git worktree remove ~/Worktrees/${WTPATH}
-	EMPTYCOUNT=$(find ~/Worktrees/${REPONAME} -maxdepth 1 -mindepth 1 -empty | sed "s/${SEDHOME}\/Worktrees\///" | wc -l | awk '{ print $1 }')
-	if [[ "${EMPTYCOUNT}" != "0" ]]; then
-          DIRTODELETE=$(find ~/Worktrees/${REPONAME} -maxdepth 1 -mindepth 1 -empty | sed "s/${SEDHOME}\/Worktrees\///" | fzf --prompt "Remove Empty Directory? (ENTER/delete, ESC/cancel) >")
-	  if [[ -n ${DIRTODELETE} ]]; then
-	    if [[ -d ~/Worktrees/${DIRTODELETE} ]]; then
-	      rmdir ~/Worktrees/${DIRTODELETE}
-	    else
-	      echo -e "${YELLOW}This shouldn't happen, the directory didn't exist!${NONE}"
-	    fi
-	  else
+        JIRA=$(dirname $(dirname ${WTPATH})) # JIRA is two paths up
+        EMPTYCOUNT=$(find ~/Worktrees/${JIRA}/${REPONAME} -maxdepth 0 -mindepth 0 -empty | sed "s/${SEDHOME}\/Worktrees\///" | wc -l | awk '{ print $1 }')
+        if [[ "${EMPTYCOUNT}" != "0" ]]; then
+          DIRTODELETE=$(find ~/Worktrees/${JIRA}/${REPONAME} -maxdepth 0 -mindepth 0 -empty | sed "s/${SEDHOME}\/Worktrees\///" | fzf --prompt "Remove Empty Directory? (ENTER/delete, ESC/cancel) >")
+          if [[ -n ${DIRTODELETE} ]]; then
+            if [[ -d ~/Worktrees/${DIRTODELETE} ]]; then
+              rmdir ~/Worktrees/${DIRTODELETE}
+            else
+              echo -e "${YELLOW}This shouldn't happen, the directory didn't exist!${NONE}"
+            fi
+          else
             echo -e "${YELLOW}Deletion of empty dirctory cancelled${NONE}"
-	  fi
-	fi
+          fi
+        fi
       else
         echo -e "${YELLOW}The worktree directory doesn't exist, aborting${NONE}"
       fi
