dcd402fd20dd926f55c6786aa8731639e22145a2
cmaahs <cmaahs@gmail.com>
2022-01-28 17:33:59 -0600

    fix a hardcode in save-session

--- bin/bash_functions.sh
+++ bin/bash_functions.sh
@@ -5,7 +5,7 @@ function save-session {
     chmod 755 ~/Work/${ITERM_PROFILE}/restore-session.sh
   fi
   echo "ttab -s ${ITERM_PROFILE} 'cd $(pwd); set-title-tab ${ITERM_TAB}'" >> ~/Work/${ITERM_PROFILE}/restore-session.sh
-  cat ~/Work/TCDE-81/restore-session.sh | sort -u | sponge ~/Work/${ITERM_PROFILE}/restore-session.sh
+  cat ~/Work/${ITERM_PROFILE}/restore-session.sh | sort -u | sponge ~/Work/${ITERM_PROFILE}/restore-session.sh
   exit
 }
 alias gj='save-session'
