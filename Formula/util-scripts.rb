7b63dc7c0783baee7a4afbfdfc41c967ed9d9b59
cmaahs <cmaahs@gmail.com>
2022-01-28 18:14:50 -0600

    New mk-window-title, fixed JSON extra comma

--- Formula/util-scripts.rb
+++ Formula/util-scripts.rb
@@ -2,7 +2,7 @@ class UtilScripts < Formula
   desc "This is a collection of general utility scripts"
   homepage "https://github.com/cmaahs/homebrew-admin-scripts"
   url "https://github.com/cmaahs/homebrew-admin-scripts.git"
-  version "0.0.76"
+  version "0.0.77"
 
   def install
     bin.install "bin/check-docker-tag"
