0933bf89c461623dc71a88638849652cc7108a8c
cmaahs <cmaahs@gmail.com>
2022-03-15 20:27:02 -0500

    Add ss and x to save-session

--- bin/bash_functions.sh
+++ bin/bash_functions.sh
@@ -1,11 +1,15 @@
 # requires ttab (https://github.com/mklement0/ttab) for restoration
 function save-session {
+  CLOSE_TAB=${1}
   if [[ ! -f ~/Work/${ITERM_PROFILE}/restore-session.sh ]]; then
     touch ~/Work/${ITERM_PROFILE}/restore-session.sh
     chmod 755 ~/Work/${ITERM_PROFILE}/restore-session.sh
   fi
   echo "ttab -s ${ITERM_PROFILE} 'cd $(pwd); set-title-tab ${ITERM_TAB}'" >> ~/Work/${ITERM_PROFILE}/restore-session.sh
   cat ~/Work/${ITERM_PROFILE}/restore-session.sh | sort -u | sponge ~/Work/${ITERM_PROFILE}/restore-session.sh
-  exit
+  if [[ "${CLOSE_TAB}" == "x" ]]; then
+    exit
+  fi
 }
-alias gj='save-session'
+alias gj='save-session x'
+alias ss='save-session'
