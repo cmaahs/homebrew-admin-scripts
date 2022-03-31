47c24a6c24f59687460ba20eae8406d163928822
cmaahs <cmaahs@gmail.com>
2022-03-31 15:33:50 -0500

    Update helm ecr functions

--- Formula/util-scripts.rb
+++ Formula/util-scripts.rb
@@ -2,7 +2,7 @@ class UtilScripts < Formula
   desc "This is a collection of general utility scripts"
   homepage "https://github.com/cmaahs/homebrew-admin-scripts"
   url "https://github.com/cmaahs/homebrew-admin-scripts.git"
-  version "0.0.87"
+  version "0.0.88"
 
   def install
     bin.install "bin/check-docker-tag"
--- bin/helm_ecr_functions.sh
+++ bin/helm_ecr_functions.sh
@@ -1,10 +1,17 @@
-function helm-login-ecr {
-export HELM_EXPERIMENTAL_OCI=1
-LOGIN_PASSWORD=$(aws ecr get-login-password --region us-west-2)
-if [[ "${AWS_PROFILE}" == "core-shared-services" ]]; then
-  REGISTRY=712053168757.dkr.ecr.us-west-2.amazonaws.com
-else
-  REGISTRY=497653049893.dkr.ecr.us-west-2.amazonaws.com
-fi
-helm registry login ${REGISTRY} --username AWS --password ${LOGIN_PASSWORD}
+helm-login-ecr () {
+  export HELM_EXPERIMENTAL_OCI=1
+  local AWS_ACCOUNT_ALIAS=$(aws iam list-account-aliases | jq -r '.AccountAliases[0]')
+  if [[ "${AWS_ACCOUNT_ALIAS}" =~ "core-shared-services" ]]; then
+    local REGISTRY_ID=$(aws ecr describe-registry | jq -r '.registryId')
+    if [[ -n "${REGISTRY_ID}" ]]; then
+      local REGISTRY=${REGISTRY_ID}.dkr.ecr.us-west-2.amazonaws.com
+      local LOGIN_PASSWORD=$(aws ecr get-login-password)
+      echo "Authenticating to ${REGISTRY}"
+      helm registry login ${REGISTRY} --username AWS --password ${LOGIN_PASSWORD}
+    else
+      echo "Could not determine the registry id"
+    fi
+  else 
+    echo "Your AWS profile is not pointing to a core-shared-services account"
+  fi
 }
